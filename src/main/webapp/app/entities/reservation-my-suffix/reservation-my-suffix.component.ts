99450e410813346bbfadc0d41b9ae02134111664
Pavlos Dedousis <pdedou@hcmr.gr>
2018-09-28 15:42:45 +0300

    npm install --save @angular/animations angularx-flatpickr
    Integration of angular-calendar
    (https://mattlewis92.github.io/angular-calendar/#/kitchen-sink) on
    reservation-my-suffix.component

--- src/main/webapp/app/entities/reservation-my-suffix/reservation-my-suffix.component.ts
+++ src/main/webapp/app/entities/reservation-my-suffix/reservation-my-suffix.component.ts
@@ -10,11 +10,56 @@ import { Principal } from 'app/core';
 import { ITEMS_PER_PAGE } from 'app/shared';
 import { ReservationMySuffixService } from './reservation-my-suffix.service';
 
+import {
+    ChangeDetectionStrategy,
+    ViewChild,
+    TemplateRef
+} from '@angular/core';
+import {
+    startOfDay,
+    endOfDay,
+    subDays,
+    addDays,
+    endOfMonth,
+    isSameDay,
+    isSameMonth,
+    addHours
+} from 'date-fns';
+import { Subject } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+    CalendarEvent,
+    CalendarEventAction,
+    CalendarEventTimesChangedEvent,
+    CalendarView
+} from 'angular-calendar';
+
+const colors: any = {
+    red: {
+        primary: '#ad2121',
+        secondary: '#FAE3E3'
+  },
+    blue: {
+        primary: '#1e90ff',
+        secondary: '#D1E8FF'
+  },
+    yellow: {
+        primary: '#e3bc08',
+        secondary: '#FDF1BA'
+  }
+};
+
 @Component({
     selector: 'jhi-reservation-my-suffix',
     templateUrl: './reservation-my-suffix.component.html'
 })
 export class ReservationMySuffixComponent implements OnInit, OnDestroy {
+    @ViewChild('modalContent')
+    modalContent: TemplateRef<any>;
+    view: CalendarView = CalendarView.Month;
+    CalendarView = CalendarView;
+    viewDate: Date = new Date();
+
     reservations: IReservationMySuffix[];
     currentAccount: any;
     eventSubscriber: Subscription;
@@ -27,13 +72,79 @@ export class ReservationMySuffixComponent implements OnInit, OnDestroy {
     totalItems: number;
     currentSearch: string;
 
+    modalData: {
+        action: string;
+        event: CalendarEvent;
+    };
+
+    actions: CalendarEventAction[] = [
+    {
+        label: '<i class="fa fa-fw fa-pencil"></i>',
+        onClick: ({ event }: { event: CalendarEvent }): void => {
+        this.handleEvent('Edited', event);
+        }
+    },
+    {
+    label: '<i class="fa fa-fw fa-times"></i>',
+    onClick: ({ event }: { event: CalendarEvent }): void => {
+        this.events = this.events.filter(iEvent => iEvent !== event);
+        this.handleEvent('Deleted', event);
+      }
+    }
+    ];
+
+    refresh: Subject<any> = new Subject();
+
+    events: CalendarEvent[] = [
+    {
+        start: subDays(startOfDay(new Date()), 1),
+        end: addDays(new Date(), 1),
+        title: 'A 3 day event',
+        color: colors.red,
+        actions: this.actions,
+        allDay: true,
+        resizable: {
+        beforeStart: true,
+        afterEnd: true
+    },
+    draggable: true
+    },
+    {
+        start: startOfDay(new Date()),
+        title: 'An event with no end date',
+        color: colors.yellow,
+        actions: this.actions
+    },
+    {
+        start: subDays(endOfMonth(new Date()), 3),
+        end: addDays(endOfMonth(new Date()), 3),
+        title: 'A long event that spans 2 months',
+        color: colors.blue,
+        allDay: true
+    },
+    {
+        start: addHours(startOfDay(new Date()), 2),
+        end: new Date(),
+        title: 'A draggable and resizable event',
+        color: colors.yellow,
+        actions: this.actions,
+        resizable: {
+        beforeStart: true,
+        afterEnd: true
+        },
+        draggable: true
+    }
+    ];
+    activeDayIsOpen: boolean = true;
+
     constructor(
         private reservationService: ReservationMySuffixService,
         private jhiAlertService: JhiAlertService,
         private eventManager: JhiEventManager,
         private parseLinks: JhiParseLinks,
         private activatedRoute: ActivatedRoute,
-        private principal: Principal
+        private principal: Principal,
+        private modal: NgbModal
     ) {
         this.reservations = [];
         this.itemsPerPage = ITEMS_PER_PAGE;
@@ -47,6 +158,51 @@ export class ReservationMySuffixComponent implements OnInit, OnDestroy {
             this.activatedRoute.snapshot && this.activatedRoute.snapshot.params['search']
                 ? this.activatedRoute.snapshot.params['search']
                 : '';
+    }
+
+    dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    if (isSameMonth(date, this.viewDate)) {
+        this.viewDate = date;
+        if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+        )   {
+        this.activeDayIsOpen = false;
+        }   else {
+        this.activeDayIsOpen = true;
+        }
+    }
+    }
+
+    eventTimesChanged({
+    event,
+    newStart,
+    newEnd
+    }:  CalendarEventTimesChangedEvent): void {
+    event.start = newStart;
+    event.end = newEnd;
+    this.handleEvent('Dropped or resized', event);
+    this.refresh.next();
+    }
+
+    handleEvent(action: string, event: CalendarEvent): void {
+    this.modalData = { event, action };
+    this.modal.open(this.modalContent, { size: 'lg' });
+    }
+
+    addEvent(): void {
+    this.events.push({
+        title: 'New event',
+        start: startOfDay(new Date()),
+        end: endOfDay(new Date()),
+        color: colors.red,
+        draggable: true,
+        resizable: {
+        beforeStart: true,
+        afterEnd: true
+        }
+    });
+    this.refresh.next();
     }
 
     loadAll() {
--- src/main/webapp/app/entities/reservation-my-suffix/reservation-my-suffix.module.ts
+++ src/main/webapp/app/entities/reservation-my-suffix/reservation-my-suffix.module.ts
@@ -1,5 +1,12 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { ReservationsystemSharedModule } from 'app/shared';
 import {
@@ -15,7 +22,16 @@ import {
 const ENTITY_STATES = [...reservationRoute, ...reservationPopupRoute];
 
 @NgModule({
-    imports: [ReservationsystemSharedModule, RouterModule.forChild(ENTITY_STATES)],
+    imports: [ReservationsystemSharedModule,
+	    CommonModule,
+        FormsModule,
+        NgbModalModule,
+        FlatpickrModule.forRoot(),
+        CalendarModule.forRoot({
+          provide: DateAdapter,
+          useFactory: adapterFactory
+        }),
+		RouterModule.forChild(ENTITY_STATES)],
     declarations: [
         ReservationMySuffixComponent,
         ReservationMySuffixDetailComponent,
